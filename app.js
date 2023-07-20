df5d6e405a2926a9c056e84c00fd7233f7f04dad
ivan.cinho <pabloivan84@gmail.com>
2023-07-20 11:06:53 -0300

    20/7

---
+++ app.js
@@ -0,0 +1,72 @@
+
+const correo = document.querySelector(".fa-envelope");
+const wa = document.querySelector(".fa-whatsapp");
+const github = document.querySelector(".fa-github");
+const linkedin = document.querySelector(".fa-linkedin");
+const phone = document.querySelector(".fa-phone");
+const botonCarta = document.querySelector(".botonCarta");
+const botonFormacion = document.querySelector(".botonFormacion")
+const botonExperiencia = document.querySelector(".botonExperiencia");
+const botonReferencia = document.querySelector(".botonReferencia");
+const botonCertificaciones = document.querySelector(".botonCertificaciones");
+const Carta = document.querySelector(".Carta");
+const Formacion = document.querySelector(".Formacion");
+const Experiancia = document.querySelector(".Experiancia");
+const Referencias = document.querySelector(".Referencias");
+const Certificaciones = document.querySelector(".Certificaciones")
+
+botonFormacion.addEventListener("click", ()=> {
+    Formacion.style.display = 'block'
+    Carta.style.display = 'none'
+    Experiancia.style.display = 'none'
+    Referencias.style.display = 'none'
+    Certificaciones.style.display = "none"
+
+})
+
+botonExperiencia.addEventListener("click", ()=> {
+    Formacion.style.display = 'none'
+    Carta.style.display = 'none'
+    Experiancia.style.display = 'block'
+    Referencias.style.display = 'none'
+    Certificaciones.style.display = "none"
+
+})
+
+botonReferencia.addEventListener("click", ()=> {
+    Formacion.style.display = 'none'
+    Carta.style.display = 'none'
+    Experiancia.style.display = 'none'
+    Referencias.style.display = 'block'
+    Certificaciones.style.display = "none"
+
+})
+
+botonFormacion.addEventListener("click", ()=> {
+    Formacion.style.display = 'block'
+    Carta.style.display = 'none'
+    Experiancia.style.display = 'none'
+    Referencias.style.display = 'none'
+    Certificaciones.style.display = "none"
+
+})
+
+botonCertificaciones.addEventListener("click", ()=> {
+    Formacion.style.display = 'none'
+    Carta.style.display = 'none'
+    Experiancia.style.display = 'none'
+    Referencias.style.display = 'none'
+    Certificaciones.style.display = "block"
+
+})
+
+botonCarta.addEventListener("click", ()=> {
+    Formacion.style.display = 'none'
+    Carta.style.display = 'block'
+    Experiancia.style.display = 'none'
+    Referencias.style.display = 'none'
+    Certificaciones.style.display = "none"
+
+})
+
+// const Formacion = document.querySelector(".Formacion").style.display = 'block';
