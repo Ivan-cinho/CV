066159952650b96aacf078fa3ed7392ebf67d0ee
ivan.cinho <pabloivan84@gmail.com>
2023-11-03 11:24:55 -0300

    3/11

---
+++ arrays.js
@@ -0,0 +1,17 @@
+
+
+
+
+const datosFormacion = [{lugar: "E.E.T. nro 11 Islas Malvinas", descripcion: "1998-2002 Técnico electromecánico"},
+                        {lugar: "Escuela de musica del sindicato argentino de musicos", descripcion: "2004-2008"},
+                        {lugar: "Escuela de música popular de Avellaneda", descripcion: "2009-2010"}
+]
+
+const datosExperiencia = [{lugar: "Cat-Technologies", descripcion: "Desde abril de 2022 a la fecha desempeñando el puesto de operador de soporte técnico."},
+                          {lugar: "Cardinal", descripcion: "Desde diciembre de 2021 a marzo del 2022 desempeñando el puesto de operador de asistencia al viajero."},
+                          {lugar: "IGS Argentina", descripcion: "Desde Abril del 2015 a septiembre de 2019 desempeñando el puesto de coordinador de urgencias, emergencias y back office."},  
+                          {lugar: "TNS-Gallup", descripcion: "Desde octubre del 2012 a Abril del 2015 desempeñando el puesto de encuestador telefónico y back office."},  
+                          {lugar: "Conexia S.A.", descripcion: "Desde enero del 2011 a Abril del 2012 desempeñando el puesto de operador de mesa de ayuda a prestadores de obras sociales."},  
+                          {lugar: "Telmark S.A.", descripcion: "Desde marzo del 2010 a diciembre de 2010 desempeñando el puesto de operador telefónico de soporte técnico."},  
+                          {lugar: "Independiente", descripcion: "Desde noviembre de 2007 a la fecha dando clases particulares de música."},  
+                          {lugar: "Mindsoft S.A.", descripcion: "Desde agosto del 2004 hasta diciembre de 2007 desempeñando el puesto de cadete en todo el AMBA Y CABA."}]
