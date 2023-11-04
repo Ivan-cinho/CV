2c78130329b477be058cbb9199fb2aa4e910411a
ivan.cinho <pabloivan84@gmail.com>
2023-11-04 12:26:59 -0300

    4/11

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const botonCertificaciones = document.querySelector(".botonCertificaciones");
 const Carta = document.querySelector(".Carta");
 const Formacion = document.querySelector(".Formacion");
 const Experiancia = document.querySelector(".Experiancia");
-const accordionExperiencia = document.querySelector(".accordionExperiencia")
 const Referencias = document.querySelector(".Referencias");
 const Certificaciones = document.querySelector(".Certificaciones")
 
@@ -72,65 +71,33 @@ botonCarta.addEventListener("click", ()=> {
 
 })
 
-// bloque de acordeon del gallego
-// function cargarDatos(accordion, array) {
-//     if (array.length > 0) {
-//         array.forEach(elemento => {
-//             accordion.innerHTML += `<div class="acordeon">
-//                                         <div class="bloque activo">
-//                                         <i class="fa-regular fa-chevron-down"></i>
-//                                             <h4 class="h4">${elemento.lugar}</h4>
-//                                             <p class="contenido">${elemento.descripcion}</p>
-//                                         </div>
-//                                     </div>`
-//         });
-//     }
-// }
-// cargarDatos(Experiancia, datosExperiencia)
-// cargarDatos(Formacion, datosFormacion)
 
-// const bloque = document.querySelectorAll(".bloque")
-// const h4 = document.querySelectorAll(".h4")
+// carga de acordeones de las secciones formacion y experiencia
+const acordeonExperiencia = document.querySelector("#acordeonExperiencia")
+const acordeonFormacion = document.querySelector("#acordeonFormacion")
 
-// h4.forEach( ( cadaH4 , i)=> {
-//     h4[i].addEventListener('click', ()=> {
-//         bloque.forEach( ( cadaBloque, i)=> {
-//             bloque[i].classList.remove('activo')
-//         })
-//         bloque[i].classList.add('activo')
-//     })
-// })
-
-
-// bloque de acordeon del mexicano
-
-function cargarDatos(accordion, array) {
+function cargarAcordeon(acordeon, array) {
     if (array.length > 0) {
         array.forEach(elemento => {
-            accordion.innerHTML += `<div id="contenedorAcordeon">
-                                        <nav class="menu">
-                                            <ul>
-                                                <li class="list">
-                                                    <div class="otraClase">
-                                                        <a href="#" class="lugar">${elemento.lugar}</a><i class="fa-regular fa-chevron-down"></i>
-                                                    </div>
-                                                    <p class="descripcion">${elemento.descripcion}</p>
-                                                </li>
-                                            </ul>
-                                        </nav>
-                                    </div>`
+            acordeon.innerHTML += `<li class="list">
+                                        <div class="otraClase">
+                                            <a href="#" class="lugar">${elemento.lugar}</a><i class="fa-regular fa-chevron-down"></i>
+                                        </div>
+                                        <p class="descripcion">${elemento.descripcion}</p>
+                                    </li>`
         });
     }
 }
-cargarDatos(Experiancia, datosExperiencia)
-cargarDatos(Formacion, datosFormacion)
+cargarAcordeon(acordeonExperiencia, datosExperiencia)
+cargarAcordeon(acordeonFormacion, datosFormacion)
 
-const acordeon = document.querySelectorAll(".acordeon")
-const contenedorAcordeon = document.getElementById("contenedorAcordeon")
+function animarAcordeon(acordeon){
+    acordeon.addEventListener('click', (e)=> {
+        if (e.target.classList.contains('otraClase')){
+            e.target.parentElement.classList.toggle('scale')
+        }
+    })
+}
+animarAcordeon(acordeonExperiencia)
+animarAcordeon(acordeonFormacion)
 
-
-contenedorAcordeon.addEventListener('click', (e)=> {
-    if (e.target.classList.contains('otraClase')){
-        e.target.parentElement.classList.toggle('scale')
-    }
-})
